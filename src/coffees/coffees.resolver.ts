e7ad3fa65c45f48d088af9d04ecfdf812e7076c9
hugocruzlfc <hugocruzlfc@gmail.com>
2024-02-28 13:16:48 +0100

    feat: adding coffees service

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,10 +1,26 @@
-import { Query, Resolver } from '@nestjs/graphql';
-import { Coffee } from 'src/graphql-types';
+import { ParseIntPipe } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Coffee, CreateCoffeeInput } from 'src/graphql-types';
+import { CoffeesService } from './coffees.service';
 
 @Resolver()
 export class CoffeesResolver {
+  constructor(private readonly coffeeService: CoffeesService) {}
+
   @Query('coffees')
   async findAll(): Promise<Coffee[]> {
-    return [];
+    return this.coffeeService.findAll();
+  }
+
+  @Query('coffee')
+  async findOne(@Args('id', ParseIntPipe) id: number): Promise<Coffee> {
+    return this.coffeeService.findOne(id);
+  }
+
+  @Mutation('createCoffee') // notice we're decoupled the name from the actual GQL mutation name 'createCoffee'
+  async create(
+    @Args('createCoffeeInput') createCoffeeInput: CreateCoffeeInput,
+  ): Promise<Coffee> {
+    return this.coffeeService.create(createCoffeeInput);
   }
 }
