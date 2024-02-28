3f69552cd592a58bbad7a229310e9b6ab71c1ff9
hugocruzlfc <hugocruzlfc@gmail.com>
2024-02-28 17:52:03 +0100

    feat: adding respository connection

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,17 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import * as GraphQLTypes from '../graphql-types';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Coffee } from './entities';
+import { Repository } from 'typeorm';
+import { UserInputError } from 'apollo-server-express';
+import * as GraphqlTypes from '../graphql-types';
 
 @Injectable()
 export class CoffeesService {
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeesRepository: Repository<Coffee>,
+  ) {}
+
   async findAll() {
-    return [];
+    return this.coffeesRepository.find();
   }
 
-  async findOne(id: number) {
-    return null;
+  async findOne(id: number): Promise<Coffee> {
+    const coffee = await this.coffeesRepository.findOne({ where: { id } });
+    if (!coffee) {
+      // ⚠️ If you use the latest version of Apollo (>= v4), import "UserInputError" from "@nestjs/graphql"
+      // Users that still depend on Apollo v3 can import this class from the "apollo-server-express" package
+      throw new UserInputError(`Coffee #${id} does not exist`);
+    }
+    return coffee;
   }
 
-  async create(createCoffeeInput: GraphQLTypes.CreateCoffeeInput) {
-    return null;
+  async create(
+    createCoffeeInput: GraphqlTypes.CreateCoffeeInput,
+  ): Promise<Coffee> {
+    const coffee = this.coffeesRepository.create(createCoffeeInput);
+    return this.coffeesRepository.save(coffee);
   }
 }
