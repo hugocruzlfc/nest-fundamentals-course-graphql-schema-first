eb0f360f64cce14fb915fb7cf996377be3cce756
hugocruzlfc <hugocruzlfc@gmail.com>
2024-02-29 18:38:38 +0100

    feat: adding drinks interface

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,9 +4,15 @@ import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee, Flavor } from './entities';
 import { CoffeeFlavorResolver } from './coffee-flavor.resolver';
+import { DrinksResolver } from './drinks.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
-  providers: [CoffeesResolver, CoffeesService, CoffeeFlavorResolver],
+  providers: [
+    CoffeesResolver,
+    CoffeesService,
+    CoffeeFlavorResolver,
+    DrinksResolver,
+  ],
 })
 export class CoffeesModule {}
